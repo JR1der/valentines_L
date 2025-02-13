c889da25790b92a4abae0b413b36eef0803ec414
IvanPopovych <byd0663@gmail.com>
2025-02-13 20:08:33 +0100

    IMG fix

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ const vid = document.querySelector("video");
 let count = 2;
 
 const gifs = [
-  "../resources/cat-heart.gif",
-  "../resources/rusure.gif",
-  "../resources/3shocked-1.gif",
-  "../resources/4-crying.gif",
-  "../resources/5-crying.gif",
-  "../resources/idc.gif"
+  "./resources/catheart.gif",
+  "./resources/rusure.gif",
+  "./resources/3shocked1.gif",
+  "./resources/4crying.gif",
+  "./resources/5crying.gif",
+  "./resources/idc.gif"
 ];
 
 
@@ -22,21 +22,21 @@ gifs.forEach(gifSrc => {
 
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "../resources/rusure.gif";
+    gif.src = "./resources/rusure.gif";
     text.innerHTML = "Ти хотіла натиснути ТАК? 🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "../resources/3shocked-1.gif";
+    gif.src = "./resources/3shocked1.gif";
     text.innerHTML = "В тебе напевне зіскользнув палець? 🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "../resources/4-crying.gif";
+    gif.src = "./resources/4crying.gif";
     text.innerHTML = "Ти впевнена? 😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -44,7 +44,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "../resources/5-crying.gif";
+    gif.src = "./resources/5crying.gif";
     text.innerHTML = "В тебе немає інших варіантів 😏";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -54,7 +54,7 @@ no.addEventListener("click", () => {
 
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/idc.gif";
+  gif.src = "./resources/idc.gif";
   text.innerHTML = "Я знав ❤️";
   yes.innerHTML = '<a href="https://t.me/ivan_ppv">Напиши мені 🥰</a>';
   yes.style.height = "90%";
